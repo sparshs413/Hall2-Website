bd23c1c9a9818691cf50c8b1730fe3db0ab459c6
Sparsh Sihotiya <sparshs@iitk.ac.in>
2021-05-19 12:56:10 +0530

    Updated Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,14 @@
-import React, { Component, Fragment } from "react";
-import { withRouter } from "react-router-dom";
-import { Feed, Segment, List, Button, Icon, Container, Header, Image, Comment } from "semantic-ui-react";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Segment, List, Container } from 'semantic-ui-react';
 
 export class Footer extends Component {
-
 	render() {
 		return (
-			<Segment className='footer' inverted vertical style={{ margin: "0 0 0", padding: "20vh 0em" }}>
-				<Container textAlign="center">
-					<List horizontal inverted divided link size="small">
-						<List.Item style={{fontSize: '1.2em'}}>Designed by Hall 2</List.Item>
+			<Segment className='footer' inverted vertical style={{ margin: '50px 0 0', padding: '20vh 0em' }}>
+				<Container textAlign='center'>
+					<List horizontal inverted divided link size='small'>
+						<List.Item style={{ fontSize: '1.2em' }}>Designed by Hall 2</List.Item>
 					</List>
 				</Container>
 			</Segment>
