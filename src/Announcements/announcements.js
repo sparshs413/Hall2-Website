b26c507b2b8f063bad7b934d08161dd39080a970
Sparsh Sihotiya <sparshs@iitk.ac.in>
2021-05-19 12:18:25 +0530

    Changed Announcements.js

--- src/Announcements/announcements.js
+++ src/Announcements/announcements.js
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import "./announce.css";
-import { Spinner } from "react-bootstrap";
-import firebase from "../Firebase";
-import { Button, Icon } from "semantic-ui-react";
-import TimeAgo from "react-timeago";
- 
+import React, { Component } from 'react';
+import './announce.css';
+import { Spinner } from 'react-bootstrap';
+import firebase from '../Firebase';
+import { Button } from 'semantic-ui-react';
+import TimeAgo from 'react-timeago';
+
 export class Announcements extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			title: "",
-			message: "",
-			password: "",
-			from: "",
-			btn_class: "",
-			items: ["hello", "world"],
+			title: '',
+			message: '',
+			password: '',
+			from: '',
+			btn_class: '',
+			items: ['hello', 'world'],
 			Matches: [],
-			isAdmin: "",
+			isAdmin: '',
 			isLoading: true,
 			isLoadMore: false,
 			hideLoadMore: false,
-			last: ''
+			last: '',
 		};
 
 		this.componentDidMount = this.componentDidMount.bind(this);
 	}
-
 
 	componentDidMount() {
 		this.setState({ isLoading: true });
 		var lastTime = '';
 		firebase
 			.firestore()
-			.collection("announcements")
-			.orderBy("timestamp", "desc")
+			.collection('announcements')
+			.orderBy('timestamp', 'desc')
 			.limit(10)
 			.get()
 			.then((querySnapshot) => {
@@ -46,73 +44,60 @@ export class Announcements extends Component {
 					}
 					lastTime = doc.data().timestamp;
 				});
-				this.setState({last:lastTime})
 
-				this.setState({ Matches: Matches, isLoading: false, isLoadMore: false });
+				this.setState({ last: lastTime, Matches: Matches, isLoading: false, isLoadMore: false });
 			})
 			.catch((error) => {
-				console.log("Error getting documents: ", error);
+				console.log('Error getting documents: ', error);
 				this.setState({ isLoading: false, isLoadMore: false });
 			});
 	}
 
 	loadData = () => {
-
 		this.setState({ isLoadMore: true });
 		var lastTime = '';
 		firebase
 			.firestore()
-			.collection("announcements")
-			.orderBy("timestamp", "desc")
-			.startAfter(this.state.last) 
+			.collection('announcements')
+			.orderBy('timestamp', 'desc')
+			.startAfter(this.state.last)
 			.limit(10)
 			.get()
 			.then((querySnapshot) => {
 				var Matches = this.state.Matches;
-				var i=0;
+				var i = 0;
 				querySnapshot.forEach((doc) => {
-					if (doc.data() && JSON.stringify(Matches[i++].timestamp)!==JSON.stringify(doc.data().timestamp)) {
-							Matches.push(doc.data());
+					if (doc.data() && JSON.stringify(Matches[i++].timestamp) !== JSON.stringify(doc.data().timestamp)) {
+						Matches.push(doc.data());
+					} else {
+						this.setState({ hideLoadMore: true });
 					}
-					else{
-						this.setState({hideLoadMore: true})
-					}
-						lastTime = doc.data().timestamp;
+					lastTime = doc.data().timestamp;
 				});
-				if(i===0){
-					this.setState({hideLoadMore: true})
+				if (i === 0) {
+					this.setState({ hideLoadMore: true });
 				}
 
-				this.setState({last:lastTime})
-						console.log('d')
-				this.setState({ Matches: Matches, isLoading: false, isLoadMore: false });
+				this.setState({ last: lastTime, Matches: Matches, isLoading: false, isLoadMore: false });
 			})
 			.catch((error) => {
-				console.log("Error getting documents: ", error);
+				console.log('Error getting documents: ', error);
 				this.setState({ isLoading: false, isLoadMore: false });
 			});
-	}
-
-
-	// static propTypes = {
-	//   addAnnounce: PropTypes.func.isRequired,
-	//   getAnnounce: PropTypes.func.isRequired,
-	//   announces: PropTypes.array.isRequired,
-	// };
-
+	};
 
 	makeUI() {
 		if (this.state.Matches.length !== 0) {
 			return this.state.Matches.map((project) => (
-				<div class="card-body announce" style={{'whiteSpace': 'pre-wrap'}}>
-					<h4 class="card-title announce">{project.title}</h4>
-					<div className="by">{project.whom}</div>
-					<span className="time1">
-						<TimeAgo date={project.timestamp.toDate()} minPeriod="5" />
+				<div class='card-body announce' style={{ whiteSpace: 'pre-wrap' }}>
+					<h4 class='card-title announce'>{project.title}</h4>
+					<div className='by'>{project.whom}</div>
+					<span className='time1'>
+						<TimeAgo date={project.timestamp.toDate()} minPeriod='5' />
 					</span>
-					<span className="time2">{project.timestamp.toDate().toDateString()}</span>
-					<p class="card-text">
-						<span className="message">{project.announcement}</span>
+					<span className='time2'>{project.timestamp.toDate().toDateString()}</span>
+					<p class='card-text'>
+						<span className='message'>{project.announcement}</span>
 					</p>
 					<hr />
 				</div>
@@ -122,26 +107,20 @@ export class Announcements extends Component {
 
 	render() {
 		return (
-			<div className="col-sm-9 margin0 col-lg-6 m-auto announce-form1">
-				<div className="card card-body mt-4 mb-4 ">
+			<div className='col-sm-9 margin0 col-lg-6 m-auto announce-form1'>
+				<div className='card card-body mt-4 mb-4 '>
 					<h2>Announcements</h2>
 
 					{this.state.isLoading ? (
-						<div className="loader_center"  style={{'marginBottom': '15rem'}}>
-							<Spinner animation="border" variant="info" />
+						<div className='loader_center' style={{ marginBottom: '15rem' }}>
+							<Spinner animation='border' variant='info' />
 						</div>
 					) : (
 						<>
-
 							{this.makeUI()}
 
-							<div className='announce_load_more' style={this.state.hideLoadMore ? {'display': 'none'} : {'display': 'block'}}>
-								<Button 
-									disabled={this.state.isLoadMore} 
-									loading={this.state.isLoadMore} 
-									color="linkedin" 
-									onClick={this.loadData}
-								>
+							<div className='announce_load_more' style={this.state.hideLoadMore ? { display: 'none' } : { display: 'block' }}>
+								<Button disabled={this.state.isLoadMore} loading={this.state.isLoadMore} color='linkedin' onClick={this.loadData}>
 									Load More
 								</Button>
 							</div>
@@ -153,5 +132,4 @@ export class Announcements extends Component {
 	}
 }
 
-
-export default (Announcements);
+export default Announcements;
--- src/Announcements/form.js
+++ src/Announcements/form.js
@@ -1,34 +1,31 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import "./announce.css";
-import Card from "react-bootstrap/Card";
-import { Button, Modal, Spinner } from "react-bootstrap";
-import firebase from "../Firebase";
-import "./announce.css";
-import Firebase from "../Firebase";
-import TimeAgo from "react-timeago";
+import React, { Component } from 'react';
+import './announce.css';
+import Card from 'react-bootstrap/Card';
+import { Button, Modal, Spinner } from 'react-bootstrap';
+import firebase from '../Firebase';
+import './announce.css';
+import Firebase from '../Firebase';
+import TimeAgo from 'react-timeago';
 
 export class AnnounceForm extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			title: "",
-			message: "",
-			// password: "",
-			// password2: "",
-			from: "",
-			btn_class: "",
+			title: '',
+			message: '',
+			from: '',
+			btn_class: '',
 			show: false,
 			Matches: [],
 			id: [],
-			error: "",
+			error: '',
 			showDeleteModal: false,
-			deleteid: "",
+			deleteid: '',
 			isLoading: true,
 			isLoadMore: false,
 			hideLoadMore: false,
-			last: ''
+			last: '',
 		};
 		this.componentDidMount = this.componentDidMount.bind(this);
 	}
@@ -38,20 +35,24 @@ export class AnnounceForm extends Component {
 	};
 
 	handleClose = () => {
-		this.setState({
-			show: false,
-			error: "",
-			showDeleteModal: false,
-		});
-
+		this.setState(
+			{
+				show: false,
+				error: '',
+				showDeleteModal: false,
+			},
+			function () {
+				window.location.reload(false);
+			}
+		);
 	};
 
 	message = () => {
 		const { error } = this.state;
-		if (error === "Password Wrong") {
-			return <i className="fa fa-times" Style="color:red" aria-hidden="true"></i>;
-		} else if (error !== "") {
-			return <i className="fa fa-check-circle" Style="color:green"></i>;
+		if (error === 'Password Wrong') {
+			return <i className='fa fa-times' Style='color:red' aria-hidden='true'></i>;
+		} else if (error !== '') {
+			return <i className='fa fa-check-circle' Style='color:green'></i>;
 		}
 	};
 
@@ -62,7 +63,7 @@ export class AnnounceForm extends Component {
 
 	onSubmit = (e) => {
 		e.preventDefault();
-		const { title, password, message, from, deleteid } = this.state;
+		const { title, password, message, from } = this.state;
 		const announce = {
 			title,
 			password,
@@ -71,8 +72,7 @@ export class AnnounceForm extends Component {
 		};
 
 		if (this.state.isAdmin) {
-			const db = firebase.firestore();
-			db.collection("announcements").add({
+			firebase.firestore().collection('announcements').add({
 				title: announce.title,
 				whom: announce.from,
 				announcement: announce.message,
@@ -80,22 +80,21 @@ export class AnnounceForm extends Component {
 			});
 
 			this.setState({
-				title: "",
-				password: "",
-				message: "",
-				from: "",
+				title: '',
+				password: '',
+				message: '',
+				from: '',
 				show: true,
-				error: "Annnouncement Added Successfully",
+				error: 'Annnouncement Added Successfully',
 			});
-
 		} else {
 			this.setState({
-				title: "",
-				password: "",
-				message: "",
-				from: "",
+				title: '',
+				password: '',
+				message: '',
+				from: '',
 				show: true,
-				error: "Admin not logged in",
+				error: 'Admin not logged in',
 			});
 		}
 	};
@@ -103,30 +102,28 @@ export class AnnounceForm extends Component {
 	onDeleteSubmit = (e) => {
 		e.preventDefault();
 		const { deleteid, isAdmin } = this.state;
-		console.log(deleteid);
 
 		if (isAdmin) {
 			var db = firebase.firestore();
 
-			db.collection("announcements")
+			db.collection('announcements')
 				.doc(deleteid)
 				.delete()
 				.then(function () {
-					console.log("Document successfully deleted!");
+					console.log('Document successfully deleted!');
 					window.location.reload(false);
 				})
 				.catch(function (error) {
-					console.error("Error removing document: ", error);
+					console.error('Error removing document: ', error);
 				});
 			this.setState({
-				// password2: "",
-				error: "Annnouncement Deleted Successfully",
+				error: 'Annnouncement Deleted Successfully',
 				showDeleteModal: true,
 			});
 		} else {
 			this.setState({
 				showDeleteModal: true,
-				error: "Admin not Logged in",
+				error: 'Admin not Logged in',
 			});
 		}
 	};
@@ -135,22 +132,22 @@ export class AnnounceForm extends Component {
 		firebase.auth().onAuthStateChanged((user) => {
 			if (user) {
 				Firebase.firestore()
-					.collection("users-data")
-					.where("email", "==", user.email)
+					.collection('users-data')
+					.where('email', '==', user.email)
 					.get()
 					.then((querySnapshot) => {
 						var admin = false;
 
 						querySnapshot.forEach(function (doc) {
 							if (doc.data()) {
-								admin = doc.data().permissions["admin"];
+								admin = doc.data().permissions['admin'];
 							}
 						});
 
 						this.setState({ isAdmin: admin, isLoading: false });
 					})
 					.catch(function (error) {
-						console.log("Error getting documents: ", error);
+						console.log('Error getting documents: ', error);
 						this.setState({ isLoading: false });
 					});
 			} else {
@@ -165,8 +162,8 @@ export class AnnounceForm extends Component {
 		this.authListener();
 		firebase
 			.firestore()
-			.collection("announcements")
-			.orderBy("timestamp", "desc")
+			.collection('announcements')
+			.orderBy('timestamp', 'desc')
 			.limit(10)
 			.get()
 			.then((querySnapshot) => {
@@ -174,91 +171,77 @@ export class AnnounceForm extends Component {
 				const id = [];
 
 				querySnapshot.forEach((doc) => {
-					// console.log(doc.id);
 					id.push(doc.id);
 					if (doc.data()) {
 						Matches.push(doc.data());
 					}
 					lastTime = doc.data().timestamp;
 				});
-				// console.log(Matches);
 
-				this.setState({ Matches: Matches, last:lastTime });
-				this.setState({ id: id, isLoading: false });
-				// console.log(this.state.Matches);
+				this.setState({ Matches: Matches, last: lastTime, id: id, isLoading: false });
 			})
 			.catch((error) => {
-				console.log("Error getting documents: ", error);
+				console.log('Error getting documents: ', error);
 				this.setState({ isLoading: false });
 			});
-		console.log(this.state.isAdmin);
-
 	}
-
 
 	loadData = () => {
 		this.setState({ isLoadMore: true });
 		var lastTime = '';
 		firebase
 			.firestore()
-			.collection("announcements")
-			.orderBy("timestamp", "desc")
-			.startAfter(this.state.last) 
+			.collection('announcements')
+			.orderBy('timestamp', 'desc')
+			.startAfter(this.state.last)
 			.limit(10)
 			.get()
 			.then((querySnapshot) => {
 				const Matches = this.state.Matches;
 				const id = this.state.id;
-				var i=0;
+				let i = 0;
 
 				querySnapshot.forEach((doc) => {
 					i++;
-					// console.log(doc.id);
-					if(!id.includes(doc.id)){
+					if (!id.includes(doc.id)) {
 						id.push(doc.id);
 						if (doc.data()) {
 							Matches.push(doc.data());
 						}
-					}
-					else{
-						this.setState({hideLoadMore: true})
+					} else {
+						this.setState({ hideLoadMore: true });
 					}
 
-						lastTime = doc.data().timestamp;
+					lastTime = doc.data().timestamp;
 				});
-				// console.log(Matches);
-				if(i===0){
-					this.setState({hideLoadMore: true})
+				if (i === 0) {
+					this.setState({ hideLoadMore: true });
 				}
-				this.setState({ Matches: Matches, last:lastTime });
-				this.setState({ id: id, isLoading: false, isLoadMore: false });
-				// console.log(this.state.Matches);
+				this.setState({ Matches: Matches, last: lastTime, id: id, isLoading: false, isLoadMore: false });
 			})
 			.catch((error) => {
-				console.log("Error getting documents: ", error);
+				console.log('Error getting documents: ', error);
 				this.setState({ isLoading: false, isLoadMore: false });
 			});
-	}
-
+	};
 
 	makeUI() {
 		let a = 0;
 		if (this.state.Matches.length !== 0) {
 			return this.state.Matches.map((project) => (
 				<div>
-					<div class="card-body announce" style={{'whiteSpace': 'pre-wrap'}}>
-						<button onClick={this.handleShow.bind(this, this.state.id[a++])} className="close" data-toggle="modal" data-target="#myModal" aria-label="close">
-							<span aria-hidden="true">&times;</span>
+					<div class='card-body announce' style={{ whiteSpace: 'pre-wrap' }}>
+						<button onClick={this.handleShow.bind(this, this.state.id[a++])} className='close' data-toggle='modal' data-target='#myModal' aria-label='close'>
+							<span aria-hidden='true'>&times;</span>
 						</button>
-						{/* {console.log(project)} */}
-						<h4 class="card-title announce">{project.title}</h4>
-						<div className="by">{project.whom}</div>
-						<span className="time1">
-							<TimeAgo date={project.timestamp.toDate()} minPeriod="5" />
+						<h4 class='card-title announce'>{project.title}</h4>
+						<div className='by'>{project.whom}</div>
+						<span className='time1'>
+							<TimeAgo date={project.timestamp.toDate()} minPeriod='5' />
 						</span>
-						<span className="time2">{project.timestamp.toDate().toDateString()}</span>
-						<p class="card-text">
-							<span className="message">{project.announcement}</span>
+						<span className='time2'>{project.timestamp.toDate().toDateString()}</span>
+						<p class='card-text'>
+							<span className='message'>{project.announcement}</span>
 						</p>
 						<hr />
 					</div>
@@ -270,48 +253,46 @@ export class AnnounceForm extends Component {
 	render() {
 		return (
 			<div>
-				<div className="col-sm-12 col-lg-6 m-auto announce_form1">
-					<div className="card card-body mt-4 mb-4 announce_form">
+				<div className='col-sm-12 col-lg-6 m-auto announce_form1'>
+					<div className='card card-body mt-4 mb-4 announce_form'>
 						<Card.Header>
-							<span className="announce_form_head">
+							<span className='announce_form_head'>
 								<h4> Add Announcements</h4>
 							</span>
 						</Card.Header>
 
 						<Card.Body>
 							<form onSubmit={this.onSubmit}>
-								<div className="form-check ">
+								<div className='form-check '>
 									<fieldset>
-										<div className="form-group mt-3">
-											<input className="form-control" type="text" name="title" onChange={this.onChange} value={this.state.title} placeholder="Title" />
+										<div className='form-group mt-3'>
+											<input className='form-control' type='text' name='title' onChange={this.onChange} value={this.state.title} placeholder='Title' />
 										</div>
-										<div className="form-group">
+										<div className='form-group'>
 											<textarea
-												className="form-control"
-												type="text"
-												name="message"
-												Style="height:150px"
+												className='form-control'
+												type='text'
+												name='message'
+												Style='height:150px'
 												onChange={this.onChange}
 												value={this.state.message}
 												required
-												placeholder="Announcement"
+												placeholder='Announcement'
 											/>
 										</div>
-										<div className="form-group mt-3">
+										<div className='form-group mt-3'>
 											<input
-												className="form-control"
-												type="text"
-												name="from"
+												className='form-control'
+												type='text'
+												name='from'
 												onChange={this.onChange}
 												value={this.state.from}
-												placeholder="By (e.g. Mess secretary, President etc)"
+												placeholder='By (e.g. Mess secretary, President etc)'
 											/>
 										</div>
-										{/* <div className="form-group">
-											<input className="form-control" type="password" name="password" onChange={this.onChange} value={this.state.password} placeholder="Password" required />
-										</div> */}
-										<div className="form-group">
-											<button type="submit" className="btn btn-primary announce_submit">
+
+										<div className='form-group'>
+											<button type='submit' className='btn btn-primary announce_submit'>
 												Submit
 											</button>
 										</div>
@@ -320,81 +301,33 @@ export class AnnounceForm extends Component {
 							</form>
 						</Card.Body>
 
-						<Modal show={this.state.show} className="announce_form_modal" onHide={this.handleClose}>
+						<Modal show={this.state.show} className='announce_form_modal' onHide={this.handleClose}>
 							<Modal.Header closeButton></Modal.Header>
 							<Modal.Body>
 								{this.message()}
 								{this.state.error}
 							</Modal.Body>
 							<Modal.Footer>
-								<Button variant="primary" onClick={this.handleClose}>
+								<Button variant='primary' onClick={this.handleClose}>
 									Close
 								</Button>
 							</Modal.Footer>
 						</Modal>
 					</div>
 
-					<div className="card card-body mt-4 mb-4 form">
+					<div className='card card-body mt-4 mb-4 form'>
 						<h3>All Announcements</h3>
-						{/* {this.props.announces.map((announce) => (
-              <div class="card-body announce">
-                <button
-                  onClick={this.handleShow.bind(
-                    this
-                    project.email
-                  )}
-                  className="close"
-                  data-toggle="modal"
-                  data-target="#myModal"
-                  aria-label="close"
-                >
-                  <span aria-hidden="true close">&times;</span>
-                </button>
-                <h4 class="card-title announce">{announce.title}</h4>
-                <div className="by">{announce.from} </div>
-                <span className="time2">time, date </span>
-                <p class="card-text">
-                  <span className="message">{announce.message}</span>
-                </p>
-                <hr />
-              </div>
-            ))} */}
-						{/* <div class="card-body announce">
-              <button
-                onClick={this.handleShow}
-                className="close"
-                data-toggle="modal"
-                data-target="#myModal"
-                aria-label="close"
-              >
-                <span aria-hidden="true close">&times;</span>
-              </button>
-              <h4 class="card-title announce">announce.title</h4>
-              <div className="by">announce.from </div>
-              <span className="time2">time, date </span>
-              <p class="card-text">
-                <span className="message">
-                  message mmmmmmmmmmmmmm mmmmmmmmm mmmm n n n mmmmm mmmmmmm
-                  mmmmm
-                </span>
-              </p>
-              <hr />
-            </div> */}
+
 						{this.state.isLoading ? (
-							<div className="loader_center" style={{'marginBottom': '15rem'}}>
-								<Spinner animation="border" variant="info" />
+							<div className='loader_center' style={{ marginBottom: '15rem' }}>
+								<Spinner animation='border' variant='info' />
 							</div>
 						) : (
 							<>
 								{this.makeUI()}
-								
-								<div className='announce_load_more' style={this.state.hideLoadMore ? {'display': 'none'} : {'display': 'block'}}>
-									<Button 
-										disabled={this.state.isLoadMore} 
-										loading={this.state.isLoadMore} 
-										color="linkedin" 
-										onClick={this.loadData}
-									>
+
+								<div className='announce_load_more' style={this.state.hideLoadMore ? { display: 'none' } : { display: 'block' }}>
+									<Button disabled={this.state.isLoadMore} loading={this.state.isLoadMore} color='linkedin' onClick={this.loadData}>
 										Load More
 									</Button>
 								</div>
@@ -409,10 +342,10 @@ export class AnnounceForm extends Component {
 					</Modal.Header>
 					{this.message()} {this.state.error}
 					<Modal.Footer>
-						<Button variant="danger" onClick={this.handleClose}>
+						<Button variant='danger' onClick={this.handleClose}>
 							Close
 						</Button>
-						<Button variant="primary" onClick={this.onDeleteSubmit}>
+						<Button variant='primary' onClick={this.onDeleteSubmit}>
 							Delete it
 						</Button>
 					</Modal.Footer>
@@ -422,4 +355,4 @@ export class AnnounceForm extends Component {
 	}
 }
 
-export default (AnnounceForm);
+export default AnnounceForm;
